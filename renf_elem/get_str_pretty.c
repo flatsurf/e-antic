79f6dc94385fb8bc77a504896dfc9c7eee04f7c9
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2018-03-11 12:23:09 +0100

    licence LGPL v3

--- renf_elem/get_str_pretty.c
+++ renf_elem/get_str_pretty.c
@@ -5,7 +5,7 @@
 
     e-antic is free software: you can redistribute it and/or modify it under
     the terms of the GNU Lesser General Public License (LGPL) as published
-    by the Free Software Foundation; either version 2.1 of the License, or
+    by the Free Software Foundation; either version 3.0 of the License, or
     (at your option) any later version.  See <http://www.gnu.org/licenses/>.
 */
 
