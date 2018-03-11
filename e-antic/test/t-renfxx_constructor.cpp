fa706cbcc298dacf5bcbc575e006f98d4ffdbbaa
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2018-03-11 13:46:00 +0100

    fixing header license

--- e-antic/test/t-renfxx_constructor.cpp
+++ e-antic/test/t-renfxx_constructor.cpp
@@ -5,12 +5,12 @@
 
     e-antic is free software: you can redistribute it and/or modify it under
     the terms of the GNU Lesser General Public License (LGPL) as published
-    by the Free Software Foundation; either version 2.1 of the License, or
+    by the Free Software Foundation; either version 3.0 of the License, or
     (at your option) any later version.  See <http://www.gnu.org/licenses/>.
 */
 
-#include <e-antic/renfxx.h>
 #include <flint/fmpq.h>
+#include "e-antic/renfxx.h"
 
 int main(void)
 {
