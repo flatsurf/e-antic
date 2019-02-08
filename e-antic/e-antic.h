e0a13857ab9da27b347f5aa76449cf3be80ea45c
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2019-02-08 15:18:17 +0100

    release 0.1.1

--- e-antic/e-antic.h
+++ e-antic/e-antic.h
@@ -12,8 +12,8 @@ extern "C" {
 
 #define __E_ANTIC_VERSION 0
 #define __E_ANTIC_VERSION_MINOR 1
-#define __E_ANTIC_VERSION_PATCHLEVEL 0
-#define E_ANTIC_VERSION "0.1.0"
+#define __E_ANTIC_VERSION_PATCHLEVEL 1
+#define E_ANTIC_VERSION "0.1.1"
 #define __E_ANTIC_RELEASE (__E_ANTIC_VERSION * 10000 + \
                            __E_ANTIC_VERSION_MINOR * 100 + \
                            __E_ANTIC_VERSION_PATCHLEVEL)
