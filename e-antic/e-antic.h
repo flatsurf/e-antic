3c77d8927f1187e3db2d984f412f10485e75b43e
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2019-02-08 15:21:38 +0100

    bump for next release

--- e-antic/e-antic.h
+++ e-antic/e-antic.h
@@ -12,8 +12,8 @@ extern "C" {
 
 #define __E_ANTIC_VERSION 0
 #define __E_ANTIC_VERSION_MINOR 1
-#define __E_ANTIC_VERSION_PATCHLEVEL 1
-#define E_ANTIC_VERSION "0.1.1"
+#define __E_ANTIC_VERSION_PATCHLEVEL 2
+#define E_ANTIC_VERSION "0.1.2"
 #define __E_ANTIC_RELEASE (__E_ANTIC_VERSION * 10000 + \
                            __E_ANTIC_VERSION_MINOR * 100 + \
                            __E_ANTIC_VERSION_PATCHLEVEL)
