fd02dbc9ed1e4677b2e9d3e54b21c9b5a0bc0457
Julian Rüth <julian.rueth@fsfe.org>
2020-02-08 17:30:59 +0100

    Remove HEREDOC from travis.yml
    apparently Travis CI changed recently how they parse HEREDOCs embedded
    in the travis configuration file. Strangely, it won't find out EOF
    marker anymore. So to work around, we just put the test script into an
    actual script and execute it directly instead.

---
+++ .travis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -exo pipefail
+export MAKEFLAGS="-j2"
+apt-get update
+apt-get -y install libgmp-dev libmpfr-dev libflint-dev valgrind libflint-arb-dev libboost-all-dev libtool make autoconf $PACKAGES
+./bootstrap.sh
+./configure --prefix=/usr --enable-valgrind ${CONFIGURE} || (cat config.log; false)
+make
+ldd .libs/libeantic.so
+make check || (cat `find test-suite.log`; false)
+make check-valgrind || (cat `find test-suite-memcheck.log`; false)
+make distcheck
