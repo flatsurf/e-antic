540ac42b27d5fb542e862e4eee3baead8768e104
Julian Rüth <julian.rueth@fsfe.org>
2020-06-23 18:11:40 +0200

    require antic in configure
    and drop support for FLINT 2.5.2

--- nf_elem/reduce.c
+++ nf_elem/reduce.c
@@ -80,13 +80,8 @@ void _nf_elem_reduce(nf_elem_t a, const nf_t nf)
                
                _fmpz_vec_set(r, NF_ELEM_NUMREF(a), plen);
 
-#if __FLINT_RELEASE == 20600
                _fmpz_poly_divrem(q, NF_ELEM_NUMREF(a), r, plen, 
                   fmpq_poly_numref(nf->pol), len, 0);
-#else
-               _fmpz_poly_divrem(q, NF_ELEM_NUMREF(a), r, plen,
-                  fmpq_poly_numref(nf->pol), len);
-#endif
 
                _fmpz_vec_clear(r, plen);
                _fmpz_vec_clear(q, plen - len + 1);
