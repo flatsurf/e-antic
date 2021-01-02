11886eabbefa120c34de3eca8f3601d11afe0c8f
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2021-01-02 15:27:23 +0100

    WIP: documentation

---
+++ doc/index.md
@@ -0,0 +1,31 @@
+# e-antic
+
+## Overview
+
+e-antic is a C library for computing with real embedded algebraic numbers
+with exact comparisons. It follows [flint](flintlib.org/),
+[arb](https://fredrikj.net/arb/) and [antic](https://github.com/wbhart/antic)
+naming conventions.
+
+e-antic also offers C++ and Python interfaces.
+
+## C library
+
+* installation details
+* tour of the C interface
+* index of library documentation
+
+## C++ interface
+
+* installation details
+* tour of the C++ interface
+* index of library documentation
+
+## Python interface
+
+The Python interface relies on [cppyy](https://cppyy.readthedocs.io/en/latest/)
+that provides automatic bindings to the C++ interface.
+
+* installation details
+* tour of the Python interface
+* index of library documentation
--- libeantic/e-antic/renf_elem.h
+++ libeantic/e-antic/renf_elem.h
@@ -1,3 +1,4 @@
+/// Embedded number field elements
 /*
     Copyright (C) 2016 Vincent Delecroix
 
@@ -37,6 +38,9 @@ FLINT_DLL void EANTIC_nf_elem_scalar_div_fmpq(nf_elem_t a, const nf_elem_t b, co
 FLINT_DLL void EANTIC_nf_elem_add_fmpq(nf_elem_t a, const nf_elem_t b, const fmpq_t c, const nf_t nf);
 #define nf_elem_add_fmpq(a, b, c, nf) EANTIC_nf_elem_add_fmpq(a, b, c, nf)
 
+/// === Datastructures ===
+
+/// A real embedded number field element
 typedef struct
 {
     nf_elem_t elem;    /* ANTIC number field element */
@@ -46,23 +50,15 @@ typedef struct
 typedef renf_elem_struct * renf_elem_srcptr;
 typedef renf_elem_struct renf_elem_t[1];
 
+/// === Initialization, allocation, deallocation ===
+
 void renf_elem_init(renf_elem_t a, const renf_t nf);
+
 void renf_elem_clear(renf_elem_t a, const renf_t nf);
 
 void renf_elem_set_fmpq_poly(renf_elem_t a, const fmpq_poly_t pol, const renf_t nf);
+
 void renf_elem_gen(renf_elem_t a, const renf_t nf);
-
-int renf_elem_sgn(renf_elem_t a, renf_t nf);
-void renf_elem_floor(fmpz_t a, renf_elem_t b, renf_t nf);
-void renf_elem_ceil(fmpz_t a, renf_elem_t b, renf_t nf);
-
-int renf_elem_cmp(renf_elem_t a, renf_elem_t b, renf_t nf);
-int _renf_elem_cmp_fmpq(renf_elem_t a, fmpz * num, fmpz * den, renf_t nf);
-int renf_elem_cmp_fmpq(renf_elem_t a, const fmpq_t b, renf_t nf);
-
-int renf_elem_cmp_fmpz(renf_elem_t a, const fmpz_t b, renf_t nf);
-int renf_elem_cmp_si(renf_elem_t a, const slong b, renf_t nf);
-int renf_elem_cmp_ui(renf_elem_t a, const ulong b, renf_t nf);
 
 static __inline__
 void renf_elem_swap(renf_elem_t a, renf_elem_t b)
@@ -72,46 +68,7 @@ void renf_elem_swap(renf_elem_t a, renf_elem_t b)
     *b = t;
 }
 
-double renf_elem_get_d(renf_elem_t a, renf_t nf, arf_rnd_t rnd);
-
-/* return an arb approximation of a with a relative precision at least prec */
-void renf_elem_get_arb(arb_t x, renf_elem_t a, renf_t nf, slong prec);
-
-/* set c to the continued fraction of a */
-slong renf_elem_get_cfrac(fmpz * c, renf_elem_t rem, renf_elem_t a, slong n, renf_t nf);
-
-void renf_elem_set_evaluation(renf_elem_t a, const renf_t nf, slong prec);
-
-char * renf_elem_get_str_pretty(renf_elem_t a, const char * var, renf_t nf, slong n, int flag);
-void renf_elem_print_pretty(renf_elem_t a, const char * var, renf_t nf, slong n, int flag);
-
-void renf_elem_randtest(renf_elem_t a, flint_rand_t state, mp_bitcnt_t bits, renf_t nf);
-
 void renf_elem_check_embedding(const renf_elem_t a, const renf_t nf, slong prec);
-
-int renf_elem_relative_condition_number_2exp(slong * cond, renf_elem_t a, renf_t nf);
-
-/* inline set and binary operations */
-
-int renf_elem_is_zero(const renf_elem_t a, const renf_t nf);
-
-int renf_elem_is_one(const renf_elem_t a, const renf_t nf);
-
-void renf_elem_neg(renf_elem_t a, const renf_elem_t b, const renf_t nf);
-
-int renf_elem_equal(const renf_elem_t a, const renf_elem_t b, const renf_t nf);
-
-int renf_elem_is_integer(const renf_elem_t a, const renf_t nf);
-
-int renf_elem_is_rational(const renf_elem_t a, const renf_t nf);
-
-int renf_elem_equal_si(const renf_elem_t a, const slong b, const renf_t nf);
-
-int renf_elem_equal_ui(const renf_elem_t a, const ulong b, const renf_t nf);
-
-int renf_elem_equal_fmpz(const renf_elem_t a, const fmpz_t b, const renf_t nf);
-
-int renf_elem_equal_fmpq(const renf_elem_t a, const fmpq_t b, const renf_t nf);
 
 void renf_elem_zero(renf_elem_t a, const renf_t nf);
 
@@ -132,6 +89,76 @@ void renf_elem_set_mpz(renf_elem_t a, const mpz_t c, const renf_t nf);
 void renf_elem_set_fmpq(renf_elem_t a, const fmpq_t c, const renf_t nf);
 
 void renf_elem_set_mpq(renf_elem_t a, const mpq_t c, const renf_t nf);
+
+/// === Embedding refinement ===
+
+void renf_elem_set_evaluation(renf_elem_t a, const renf_t nf, slong prec);
+
+/// === Properties and conversion ===
+
+int renf_elem_is_zero(const renf_elem_t a, const renf_t nf);
+
+int renf_elem_is_one(const renf_elem_t a, const renf_t nf);
+
+int renf_elem_is_integer(const renf_elem_t a, const renf_t nf);
+
+int renf_elem_is_rational(const renf_elem_t a, const renf_t nf);
+
+int renf_elem_sgn(renf_elem_t a, renf_t nf);
+
+void renf_elem_floor(fmpz_t a, renf_elem_t b, renf_t nf);
+
+void renf_elem_ceil(fmpz_t a, renf_elem_t b, renf_t nf);
+
+int renf_elem_relative_condition_number_2exp(slong * cond, renf_elem_t a, renf_t nf);
+
+/// === Floating point approximations ===
+
+/// Set `x` to a a real ball enclosing the element `a` that belongs to the number field `nf`
+/// with `prec` bits of precision.
+void renf_elem_get_arb(arb_t x, renf_elem_t a, renf_t nf, slong prec);
+
+/// Retourn a double approximation
+double renf_elem_get_d(renf_elem_t a, renf_t nf, arf_rnd_t rnd);
+
+/// === Comparisons ===
+
+int renf_elem_cmp(renf_elem_t a, renf_elem_t b, renf_t nf);
+
+int _renf_elem_cmp_fmpq(renf_elem_t a, fmpz * num, fmpz * den, renf_t nf);
+
+int renf_elem_cmp_fmpq(renf_elem_t a, const fmpq_t b, renf_t nf);
+
+int renf_elem_cmp_fmpz(renf_elem_t a, const fmpz_t b, renf_t nf);
+
+int renf_elem_cmp_si(renf_elem_t a, const slong b, renf_t nf);
+
+int renf_elem_cmp_ui(renf_elem_t a, const ulong b, renf_t nf);
+
+int renf_elem_equal(const renf_elem_t a, const renf_elem_t b, const renf_t nf);
+
+int renf_elem_equal_si(const renf_elem_t a, const slong b, const renf_t nf);
+
+int renf_elem_equal_ui(const renf_elem_t a, const ulong b, const renf_t nf);
+
+int renf_elem_equal_fmpz(const renf_elem_t a, const fmpz_t b, const renf_t nf);
+
+int renf_elem_equal_fmpq(const renf_elem_t a, const fmpq_t b, const renf_t nf);
+
+/// === String and printing ===
+
+char * renf_elem_get_str_pretty(renf_elem_t a, const char * var, renf_t nf, slong n, int flag);
+
+void renf_elem_print_pretty(renf_elem_t a, const char * var, renf_t nf, slong n, int flag);
+
+/// === Random ===
+
+void renf_elem_randtest(renf_elem_t a, flint_rand_t state, mp_bitcnt_t bits, renf_t nf);
+
+///=== Unary and binary operations ===
+
+/// Sets `a` to the negative of `b`
+void renf_elem_neg(renf_elem_t a, const renf_elem_t b, const renf_t nf);
 
 void renf_elem_add_si(renf_elem_t a, const renf_elem_t b, slong c, const renf_t nf);
 
@@ -165,6 +192,7 @@ void renf_elem_fmpq_sub(renf_elem_t a, const fmpq_t c, const renf_elem_t b, cons
 
 void renf_elem_mul_fmpq(renf_elem_t a, const renf_elem_t b, const fmpq_t c, const renf_t nf);
 
+/// Sets `a` to the inverse of `b`
 void renf_elem_inv(renf_elem_t a, const renf_elem_t b, const renf_t nf);
 
 void renf_elem_div_fmpq(renf_elem_t a, const renf_elem_t b, const fmpq_t c, const renf_t nf);
@@ -182,6 +210,12 @@ void renf_elem_div(renf_elem_t a, const renf_elem_t b, const renf_elem_t c, cons
 void renf_elem_fdiv(fmpz_t a, renf_elem_t b, renf_elem_t c, renf_t nf);
 
 void renf_elem_pow(renf_elem_t res, const renf_elem_t a, ulong e, const renf_t nf);
+
+/// === Other functions ===
+
+/// Set the array `c` to the `n`-th first partial quotients of the continued
+/// fraction of the element `a` of `nf`.
+slong renf_elem_get_cfrac(fmpz * c, renf_elem_t rem, renf_elem_t a, slong n, renf_t nf);
 
 #ifdef __cplusplus
 }
