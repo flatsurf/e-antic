24ec4a7bf9de8bdd4a6d0fcae637f5b11562e332
Julian Rüth <julian.rueth@fsfe.org>
2021-01-21 21:15:14 +0100

    alternatives to pword
    since it is very hard to get delimiters right in the pword approach,
    users probably need to build their own tokenization around this anyway;
    so it does not seem like we can produce a completely versatile stream
    reader.

--- libeantic/e-antic/renfxx.h
+++ libeantic/e-antic/renfxx.h
@@ -64,11 +64,11 @@ public:
 
     // Prepare an input stream to read elements living in this number field
     // from it.
-    [[deprecated("use renfxx_cereal.h instead.")]]
+    [[deprecated("use renfxx_cereal.h or renf_elem_class's constructor taking a string instead.")]]
     std::istream & set_pword(std::istream &) const;
 
     // Extract the number field stored with set_pword from an input stream.
-    [[deprecated("use renfxx_cereal.h instead.")]]
+    [[deprecated("use renfxx_cereal.h or renf_elem_class's constructor taking a string instead.")]]
     static std::shared_ptr<const renf_class> get_pword(std::istream &);
 
     std::string to_string() const;
