dc94869cda98591bd155252dd9ef3156d8563a89
Julian Rüth <julian.rueth@fsfe.org>
2021-06-14 18:33:02 +0200

    bumped version to 1.0.0-rc.15

--- doc/manual/pyeantic/conf.py
+++ doc/manual/pyeantic/conf.py
@@ -18,7 +18,7 @@ copyright = '2021, the e-antic authors'
 author = 'the e-antic authors'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0-rc.14'
+release = '1.0.0-rc.15'
 
 
 # -- General configuration ---------------------------------------------------
