fe0b7d2a23d74c25a236137558c583881e6efd46
Julian Rüth <julian.rueth@fsfe.org>
2022-12-06 01:29:45 +0200

    bumped version to 1.3.0

--- doc/manual/pyeantic/conf.py
+++ doc/manual/pyeantic/conf.py
@@ -18,7 +18,7 @@ copyright = '2021, the e-antic authors'
 author = 'the e-antic authors'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.3'
+release = '1.3.0'
 
 
 # -- General configuration ---------------------------------------------------
