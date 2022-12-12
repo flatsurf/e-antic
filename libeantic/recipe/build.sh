593ab3a1432f85d06b43fbce5cbec5a153308509
Julian Rüth <julian.rueth@fsfe.org>
2022-12-12 18:34:57 +0200

    Disable static builds for the flatsurf channel

--- libeantic/recipe/build.sh
+++ libeantic/recipe/build.sh
@@ -5,5 +5,5 @@ cd libeantic
 autoreconf -ivf
 export CXXFLAGS="-UNDEBUG -g3 $CXXFLAGS"
 export CFLAGS="-UNDEBUG -g3 $CFLAGS"
-./configure --prefix="$PREFIX" --without-benchmark --with-version-script --without-byexample
+./configure --prefix="$PREFIX" --without-benchmark --with-version-script --without-byexample --disable-static
 make install
